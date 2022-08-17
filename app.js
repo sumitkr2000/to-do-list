4130be70b943e8d6aad791067f20cb224210d6eb
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2022-08-17 23:48:58 +0530

    removed unwanted code

--- app.js
+++ app.js
@@ -18,13 +18,6 @@ const itemsSchema = {
 
 const Item = mongoose.model("Item", itemsSchema);
 
-// const listSchema = {
-//     name: String,
-//     items: []
-// };
-
-// const List = mongoose.model("List", listSchema);
-
 app.get("/", function(req, res) {
     
     const day = date.getDay();
@@ -37,23 +30,13 @@ app.get("/", function(req, res) {
 app.post("/", function(req, res) {
 
     const itemName = req.body.newItem;
-    // const listName = req.body.list;
 
     const item = new Item ({
         name: itemName
     });
 
-    // if(listName == "Today") {
-        item.save();
-        res.redirect("/");
-    // }
-    // else {
-    //     List.findOne({name: listName}, function(err, foundList) {
-    //         foundList.items.push(item);
-    //         foundList.save();
-    //         res.redirect("/" + listName);
-    //     });
-    // }
+    item.save();
+    res.redirect("/");
 });
 
 app.post("/delete", function(req, res) {
@@ -65,26 +48,6 @@ app.post("/delete", function(req, res) {
         }
     });
 });
-
-// app.get("/:pageName", function (req, res) {
-//     const pageName = req.params.pageName;
-
-//     List.findOne({name: pageName}, function(err, foundList) {
-//         if(!err) {
-//             if(!foundList) {
-//                 const list = new List ({
-//                     name: pageName,
-//                     items: Item.find({})
-//                 });
-//                 list.save();
-//                 res.redirect("/" + pageName);
-//             }
-//             else {
-//                 res.render("list", {listTitle: pageName.name, newListItems: foundList.items});
-//             }
-//         }
-//     });
-// });
 
 app.get("/about", function(req, res) {
     res.render("about");
